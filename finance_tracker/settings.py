46ca5dd62433c69e6c4a704f7b704af594e2d3b9
Abubakr <dmx0204@mail.com>
2025-05-25 14:43:01 +0500

    Another commit

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -78,9 +78,7 @@ WSGI_APPLICATION = 'finance_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 # Password validation
